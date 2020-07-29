9bd7fe7471e95fa8104c93837351fac81952e3d1
Tim Hadler <tch118@canterbury.ac.nz>
2020-07-30 11:11:30 +1200

    Plot data for tasks 1 and 2, not sure if f is calculated properley

--- main.m
+++ main.m
@@ -10,7 +10,16 @@ fs = 1024;  % Sampling frq, HZ
 n = length(data);
 t = linspace(0, n/fs, n);
 
+% Plot raw data
+figure(1)
 plot(t, data);
 title("Raw Data")
 xlabel("Time (s)");
 ylabel("Amplitude (uV)");
+
+% Find and plot spectrum
+spct = abs(fft(data));
+f = [0:n-1];   %?
+
+figure(2)
+plot(f, spct)
